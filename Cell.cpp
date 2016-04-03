2a77970e99a013ed899fc7c7e73c791022ae465b
Sergey Patrakeev <bra31k@mail.ru>
2016-04-03 16:50:08 +0500

    Update Cell.cpp

--- Cell.cpp
+++ Cell.cpp
@@ -15,15 +15,28 @@ QRectF Cell::boundingRect() const
 
 void Cell::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
 {
-    QRect rect = QRect(0, 0, 50, 50);
-    QFont font = painter->font();
-    font.setPointSize(font.pointSize()*3);
-
-    painter->drawRect(rect);
-    painter->setFont(font);
-    painter->drawText(rect,Qt::AlignCenter,m_text);
+    painter->drawRect(0,0,50,50);
+    switch (m_state) {
+    case StateX:
+       painter->drawText(25,25, "X");
+       break;
+    case State0:
+       painter->drawText(25,25, "0");
+       break;
+    default:
+       break;
 }
 
+void Cell::setState(State stat)
+{
+    if (m_state == stat)
+    {
+        return;
+    }
+    m_state=stat;
+
+    update();
+}
 
 void Cell::mousePressEvent(QGraphicsSceneMouseEvent *event)
 {
@@ -32,6 +45,5 @@ void Cell::mousePressEvent(QGraphicsSceneMouseEvent *event)
 
 void Cell::mouseReleaseEvent(QGraphicsSceneMouseEvent *event)
 {
-    m_text = "X";
-    update();
+    emit clicked(this);
 }
